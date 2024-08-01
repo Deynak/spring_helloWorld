d4c54d6a77330965cd02eab0f78bb6d1f8a3117b
Deynak <miron-ox@mail.ru>
2024-08-01 16:02:47 +0300

    second commit

---
+++ src/main/java/com/deynak/Cat.java
@@ -0,0 +1,14 @@
+package com.deynak;
+
+public class Cat {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
